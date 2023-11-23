35f8ab84d845b4eb50ec11d0eddeb6cc1f189ae3
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-23 08:53:08 +0530

    [D75-7] | Raghu K | Terraform Code for RoboShop Project.

--- main.tf
+++ main.tf
@@ -95,3 +95,12 @@ resource "aws_instance" "main" {
   }
 }
 
+resource "aws_route53_record" "record" {
+  zone_id = var.zone_id
+  name    = "${var.component}-${var.env}"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.main.private_ip]
+}
+
+
